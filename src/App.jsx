8264ffb4665f6c9b75b86a262598618603ce432b
Shavin <64917885+Singhmansingh@users.noreply.github.com>
2023-04-13 22:48:39 -0400

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Wayfinding</h1>
+
+      <header class="header">Header Stuff</header>
+
+      <main class="content">
+
+        {/* children component will replance the <Outlet/> when their path is used */ }
+
+        <Outlet/>
+
+      </main>
+
+      <footer class="navbar">Navbar Stuff</footer>
+      
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
 import './index.css'
+
+import App from './App'
+import EventDetails from './screens/EventDetails';
+
+
+// Based on https://reactrouter.com/en/main/start/tutorial
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+    
+    // Place app screens here
+    children:[
+      {
+        path:"/",
+        element:<h1>Wayfinding Homepage</h1>
+      },
+      {
+        path: "eventDetails",
+        element: <EventDetails/>
+      }
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
