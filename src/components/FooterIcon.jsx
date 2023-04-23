782676256cb5c504e63b858f845a9e4779ffa5c9
Shavin <64917885+Singhmansingh@users.noreply.github.com>
2023-04-23 16:54:04 -0400

    added navigate

--- src/components/FooterIcon.jsx
+++ src/components/FooterIcon.jsx
@@ -1,10 +1,11 @@
 // Import CSS
 import './Header.css';
-
+import { useNavigate } from 'react-router-dom';
 
 const FooterIcon = (props) => {
+  const navigate = useNavigate();
   return (
-    <div className="footer-icon">
+    <div className="footer-icon" onClick={()=>navigate('/')}>
         <img src={props.image} alt=""/>
         <p>{props.name}</p>
     </div>
